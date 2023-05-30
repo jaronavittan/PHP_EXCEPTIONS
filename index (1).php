1a7e34076e6296eea45c7018b473d7489e6cc7d7
Jaron Avittan <96601217+jaronavittan@users.noreply.github.com>
2023-05-30 12:08:46 -0500

    Add files via upload

---
+++ index (1).php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Error Handler</title>
+  </head>
+  <body>
+   <h2>Signup</h2> 
+   <form action="includes/signup.inc.php" method="POST">
+     <input type="text" name="first" placeholder="Firstname">
+     <input type="text" name="last" placeholder="Lastname">
+     <input type="text" name="email" placeholder="E-mail"> 
+     <input type="text" name="uid" placeholder="Username">
+     <input type="password" name="pwd" placeholder="Password">
+     <button type="submit" name="submit">Sign up</button>
+   </form>
+   </body>  
+</html>
